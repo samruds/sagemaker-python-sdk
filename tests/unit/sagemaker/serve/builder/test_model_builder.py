0560d8c4a3b6e276433e3a381f2cb2390e56fea7
Samrudhi Sharma <samruds@amazon.com>
2024-06-13 15:20:38 -0700

    Fix UT

--- tests/unit/sagemaker/serve/builder/test_model_builder.py
+++ tests/unit/sagemaker/serve/builder/test_model_builder.py
@@ -129,7 +129,7 @@ class TestModelBuilder(unittest.TestCase):
 
     @patch("sagemaker.serve.builder.model_builder._ServeSettings")
     @patch("sagemaker.serve.builder.model_builder.ModelBuilder._build_for_djl")
-    def test_model_server_override_dj_with_model(self, mock_build_for_djl, mock_serve_settings):
+    def test_model_server_override_djl_with_model(self, mock_build_for_djl, mock_serve_settings):
         mock_setting_object = mock_serve_settings.return_value
         mock_setting_object.role_arn = mock_role_arn
         mock_setting_object.s3_model_data_url = mock_s3_model_data_url
@@ -140,10 +140,34 @@ class TestModelBuilder(unittest.TestCase):
         mock_build_for_djl.assert_called_once()
 
     @patch("sagemaker.serve.builder.model_builder._ServeSettings")
-    @patch("sagemaker.serve.builder.model_builder.ModelBuilder._build_for_djl")
-    def test_model_server_override_dj_without_model_or_inference_spec(self, mock_build_for_djl,
+    def test_model_server_override_djl_without_model_or_inference_spec(self, mock_build_for_djl,
                                                       mock_serve_settings):
         builder = ModelBuilder(model_server=ModelServer.DJL_SERVING, model=None, inference_spec=None)
+        self.assertRaisesRegex(
+            Exception,
+            "Missing required parameter `model` or 'ml_flow' path",
+            builder.build,
+            Mode.SAGEMAKER_ENDPOINT,
+            mock_role_arn,
+            mock_session,
+        )
+
+    @patch("sagemaker.serve.builder.model_builder._ServeSettings")
+    @patch("sagemaker.serve.builder.model_builder.ModelBuilder._build_for_torchserve")
+    def test_model_server_override_torchserve_with_model(self, mock_build_for_ts,
+                                                      mock_serve_settings):
+        mock_setting_object = mock_serve_settings.return_value
+        mock_setting_object.role_arn = mock_role_arn
+        mock_setting_object.s3_model_data_url = mock_s3_model_data_url
+
+        builder = ModelBuilder(model_server=ModelServer.TORCHSERVE, model="gpt_llm_burt")
+        builder.build(sagemaker_session=mock_session)
+
+        mock_build_for_ts.assert_called_once()
+
+    @patch("sagemaker.serve.builder.model_builder._ServeSettings")
+    def test_model_server_override_torchserve_without_model_or_mlflow(self, mock_serve_settings):
+        builder = ModelBuilder(model_server=ModelServer.TORCHSERVE)
         self.assertRaisesRegex(
             Exception,
             "Missing required parameter `model` or 'ml_flow' path",
